40aa63dec0520127c4f96cc5b19a50585bcb1b5d
G0006185 <G0006185@NPRCURJB3264101.gvt.net.br>
2017-08-30 16:20:19 -0300

    db

--- src/main/java/com/telefonica/oss/pocpz/config/controller/pzcontroller.java
+++ src/main/java/com/telefonica/oss/pocpz/config/controller/pzcontroller.java
@@ -8,7 +8,6 @@ package com.telefonica.oss.pocpz.config.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
