4ec166625c1c20d9e74a55166581a2e2230ef165
G0006185 <G0006185@NPRCURJB3264101.gvt.net.br>
2017-09-01 16:24:59 -0300

    db

--- src/main/java/com/telefonica/oss/pocpz/config/DBConfiguration.java
+++ src/main/java/com/telefonica/oss/pocpz/config/DBConfiguration.java
@@ -5,8 +5,19 @@
  */
 package com.telefonica.oss.pocpz.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+import javax.naming.NamingException;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -15,7 +26,36 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @Configuration
 @EnableTransactionManagement
-@EnableJpaRepositories("com.telefonica.oss.pocpz.model.entity")
+//@EnableJpaRepositories(basePackages = "com.telefonica.oss.pocpz.model.repos")
 public class DBConfiguration {
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+    	return new JpaTransactionManager(emf);
+    }
+    @Bean
+    public DataSource dataSource() throws NamingException, PropertyVetoException  {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+	dataSource.setDriverClass("oracle.jdbc.driver.OracleDriver");
+	dataSource.setJdbcUrl("jdbc:oracle:thin:@//10.200.6.204:1521/desenv");
+	dataSource.setUser("ENG_DEV");
+	dataSource.setPassword("Vivo15#");
+	return dataSource;
+   }
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws IllegalStateException, PropertyVetoException, NamingException {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+	entityManagerFactoryBean.setDataSource(dataSource());
+	entityManagerFactoryBean.setPackagesToScan("com.telefonica.oss.pocpz.config.model.entity");
+	entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+	entityManagerFactoryBean.setJpaDialect(new HibernateJpaDialect());
+		
+	Properties jpaProterties = new Properties();
+	jpaProterties.put("hibernate.dialect", "org.hibernate.dialect.Oracle12cDialect");
+	jpaProterties.put("hibernate.hbm2ddl.auto", "update");
+	entityManagerFactoryBean.setJpaProperties(jpaProterties);
+	return entityManagerFactoryBean;
+
+    }     
+
     
 }
--- src/main/java/com/telefonica/oss/pocpz/config/controller/pzcontroller.java
+++ src/main/java/com/telefonica/oss/pocpz/config/controller/pzcontroller.java
@@ -5,6 +5,8 @@
  */
 package com.telefonica.oss.pocpz.config.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping(value="/")
 public class pzcontroller {
+    private static final Logger log = LoggerFactory.getLogger(pzcontroller.class);
+    
+    @ResponseBody
+    @RequestMapping()
+    public String Hi(){
+        log.info("passou");
+        return "Hi ;)";
+       
+    }
     @RequestMapping(value="/ola/{name}")
     @ResponseBody
     public String Hi(@PathVariable String name){
